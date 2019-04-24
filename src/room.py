7b327871365c40b6166c23d4d8b1465094fb804a
kjohna <43828763+kjohna@users.noreply.github.com>
2019-04-24 16:24:30 -0600

    set up base classes: Player, Room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+
+    def get_name(self):
+        return self.name
+
+    def get_desc(self):
+        return self.desc
